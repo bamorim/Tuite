1d0871aab64f34d4f8dfc4fa5660182f4172820d
Bernardo Amorim <contato@bamorim.com>
2021-01-10 18:28:27 -0300

    Working tweet frontend

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -4,100 +4,71 @@ open Elmish
 open Fable.Remoting.Client
 open Shared
 
-type Model = { Todos: Todo list; Input: string }
+type Model =
+    { Tweets: Tweet list
+      TweetComment: string }
 
 type Msg =
-    | GotTodos of Todo list
-    | SetInput of string
-    | AddTodo
-    | AddedTodo of Todo
+    | PostTweet
+    | TweetPosted of Tweet
+    | GotTweets of Tweet list
+    | TweetCommentChanged of string
 
 let todosApi =
     Remoting.createApi ()
     |> Remoting.withRouteBuilder Route.builder
-    |> Remoting.buildProxy<ITodosApi>
+    |> Remoting.buildProxy<ITweetsApi>
 
 let init (): Model * Cmd<Msg> =
-    let model = { Todos = []; Input = "" }
+    let model =
+        { Tweets = []
+          TweetComment = "" }
 
     let cmd =
-        Cmd.OfAsync.perform todosApi.getTodos () GotTodos
+        Cmd.OfAsync.perform todosApi.getTweets () GotTweets
 
     model, cmd
 
 let update (msg: Msg) (model: Model): Model * Cmd<Msg> =
     match msg with
-    | GotTodos todos -> { model with Todos = todos }, Cmd.none
-    | SetInput value -> { model with Input = value }, Cmd.none
-    | AddTodo ->
-        let todo = Todo.create model.Input
+    | PostTweet ->
+        let tweet =
+            { Comment = model.TweetComment
+              Username = "Frontender" }
 
         let cmd =
-            Cmd.OfAsync.perform todosApi.addTodo todo AddedTodo
+            Cmd.OfAsync.perform todosApi.postTweet tweet TweetPosted
 
-        { model with Input = "" }, cmd
-    | AddedTodo todo ->
+        { model with TweetComment = "" }, cmd
+    | GotTweets tweets -> { model with Tweets = tweets }, Cmd.none
+    | TweetPosted tweet ->
         { model with
-              Todos = model.Todos @ [ todo ] },
+              Tweets = tweet :: model.Tweets },
         Cmd.none
+    | TweetCommentChanged newComment -> { model with TweetComment = newComment }, Cmd.none
 
 open Fable.React
 open Fable.React.Props
-open Fulma
-
-let navBrand =
-    Navbar.Brand.div [] [
-        Navbar.Item.a [ Navbar.Item.Props [ Href "https://safe-stack.github.io/" ]
-                        Navbar.Item.IsActive true ] [
-            img [ Src "/favicon.png"; Alt "Logo" ]
-        ]
-    ]
-
-let containerBox (model: Model) (dispatch: Msg -> unit) =
-    Box.box' [] [
-        Content.content [] [
-            Content.Ol.ol [] [
-                for todo in model.Todos do
-                    li [] [ str todo.Description ]
-            ]
-        ]
-        Field.div [ Field.IsGrouped ] [
-            Control.p [ Control.IsExpanded ] [
-                Input.text [ Input.Value model.Input
-                             Input.Placeholder "What needs to be done?"
-                             Input.OnChange(fun x -> SetInput x.Value |> dispatch) ]
-            ]
-            Control.p [] [
-                Button.a [ Button.Color IsPrimary
-                           Button.Disabled(Todo.isValid model.Input |> not)
-                           Button.OnClick(fun _ -> dispatch AddTodo) ] [
-                    str "Add"
-                ]
-            ]
-        ]
-    ]
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    Hero.hero [ Hero.Color IsPrimary
-                Hero.IsFullHeight
-                Hero.Props [ Style [ Background
-                                         """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://unsplash.it/1200/900?random") no-repeat center center fixed"""
-                                     BackgroundSize "cover" ] ] ] [
-        Hero.head [] [
-            Navbar.navbar [] [
-                Container.container [] [ navBrand ]
-            ]
-        ]
-
-        Hero.body [] [
-            Container.container [] [
-                Column.column [ Column.Width(Screen.All, Column.Is6)
-                                Column.Offset(Screen.All, Column.Is3) ] [
-                    Heading.p [ Heading.Modifiers [ Modifier.TextAlignment(Screen.All, TextAlignment.Centered) ] ] [
-                        str "Tuite"
-                    ]
-                    containerBox model dispatch
-                ]
+    div [] [
+        ul
+            []
+            (List.map
+                (fun tweet ->
+                    li [] [
+                        span [] [
+                            str (sprintf "@%s " tweet.Username)
+                        ]
+                        span [] [ str tweet.Comment ]
+                    ])
+                model.Tweets)
+        div [] [   
+            textarea [ Value model.TweetComment
+                       OnChange(fun evt -> dispatch (TweetCommentChanged evt.Value))
+                       Placeholder "Say something..."] []
+            button [ OnClick(fun _ -> dispatch PostTweet) ] [
+                str "Tweet something"
             ]
         ]
     ]
